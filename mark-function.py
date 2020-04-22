44720968501d8d6a43a63e7555b8244cf941e0c6
João Rheinheimer <jprheinheimer50@gmail.com>
2020-04-22 22:48:31 +0200

    FTIR data analysis inside a python function, usefull for other people who wants to use the software

---
+++ mark-function.py
@@ -0,0 +1,66 @@
+"""
+MARK project:
+Degree conversion analysis
+FTIR data
+"""
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import glob
+
+
+def conversion_degreee(path, time_step, separator, header):
+    """
+    Function that makes the calculus and plot the graphic of the
+    conversion degree of polymerization about FTIR data.
+
+    Parameters:
+
+    path (string): Path to the directory with the data.
+    Example: '/home/rheinheimer/Dropbox/Codes/Python/lab-mav/mark/data/*'
+    Observation: The '*' is mandatory
+
+    time_step (int/float): Time step of the measures
+    Examples: 1, 0.2, 3.5
+
+    separator (string): The separator between the data columns
+    Examples: ';' ',' '  '
+
+    header (string): Header of the file. If none please insert 'None'
+    """
+    files = glob.glob(
+        pathname=path
+    )
+
+    degree_vector = []
+    time_vector = []
+
+    cont = 0
+    for file in sorted(files):
+        cont = cont + time_step
+        df = pd.read_csv(
+            filepath_or_buffer=file,
+            sep=separator,
+            header=header,
+            index_col=False,
+            engine='python',
+        )
+
+        df.columns = ['wave_number', 'intensity']
+        intensity = np.array(df['intensity'])
+        degree = intensity[1633]/intensity[1695]
+        degree_vector.append(degree)
+        time_vector.append(cont)
+
+    with open('degree_output.txt', 'w') as degree_output:
+        degree_output.writelines(str(degree_vector))
+
+    with open('time_step_output.txt', 'w') as time_step_output:
+        time_step_output.writelines(str(time_vector))
+
+    plt.figure()
+    plt.xlabel('Time')
+    plt.ylabel('Conversion degree')
+    plt.plot(time_vector, degree_vector)
+    plt.show()
